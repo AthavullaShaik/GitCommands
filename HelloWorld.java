5922ac64cbad1d9261d93c42f16c9b581c4fa29e
Athavulla Shaik <athavulla.shaik@finmkt.io>
2025-06-17 15:20:27 +0530

    Commited by Athavulla

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("Hello athavulla"); 
+        System.out.println("Hello athavulla");
         System.out.println("This is Git Commit");
         System.out.println("Demo Class");
     }
