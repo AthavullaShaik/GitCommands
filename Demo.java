3ce2879f2087f93de032415aa36887fa78e2c546
Athavulla Shaik <athavulla.shaik@finmkt.io>
2025-06-17 13:58:49 +0530

    Demo Class

--- Demo.java
+++ Demo.java
@@ -2,5 +2,6 @@ class Demo{
     public static void main(String[] args) {
         System.out.println("Hello athavulla");
         System.out.println("This is Git Commit");
+        System.out.println("Demo Class");
     }
 }
